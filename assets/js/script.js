a95f68cec999fc1a6f4c04e42a73caf574385e96
Ryebread5555 <Rypetty55@gmail.com>
2023-01-08 15:06:35 -0600

    parse information about current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,13 +14,24 @@ var currentHumidityEl = document.getElementById("humidity");
 function getWeather(cityName) {
     // attemping to fetch the current weather from openWeather API
     let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + cityName + "&appid=" + APIKey;
-    fetch.get(queryURL)
+    fetch(queryURL)
     .then(function (response) {
 
         todayweatherEL.classlist.remove("d-none");
 
-        
-    })
+        // This will parse to display the current weather in the area
+        const currentDate = new Date(response.data.dt * 1000);
+        const day = currentDate.getDate();
+        const month = currentDate.getMonth() + 1;
+        const year = currentDate.getFullYear();
+        nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year +") ";
+        var weatherPic = response.data.weather[0].icon;
+        currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+        currentPicEl.setAttribute("alt", response.data.weather[0].description);
+        currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F"
+        currentWindEl.innerHTML = "Wind: " + response.data.wind + " MPH";
+        currentHumidityEl.innerHTML = "Humidity: " + response.data.humidity + "%";
+            })
 }
 
 }
