0baa31d02df9ef3c79fca7e1cd65c0d3362c94b3
Ryebread5555 <Rypetty55@gmail.com>
2023-01-08 14:27:28 -0600

    added current variables

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function initPage() {
+
+// A unique API key assigned to a variable
+var APIKey = "96089c937b5fe6946fb2e0372ea5f6e2";
+
+var cityEl = document.getElementById("enter-city");
+var searchEl = document.getElementById("search-button");
+var nameEl = document.getElementById("city-name");
+var currentPicEl = document.getElementById("current-pic");
+var currentTempEl = document.getElementById("temperature");
+var currentWindEl = document.getElementById("wind");
+var currentHumidityEl = document.getElementById("humidity");
+
+}
